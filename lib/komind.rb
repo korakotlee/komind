8b1856eba10a686e8749e0acb40cda071bb467fb
Korakot Leemakdej <kleemakdej@gmail.com>
2021-06-24 22:43:54 -0700

    init

---
+++ lib/komind.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "komind/version"
+
+module Komind
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/komind_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class KomindTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Komind::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
